b3efbb15af9e695417b2128fdada54f98ab94c13
stevenluo22 <sl206@rice.edu>
2025-11-22 17:17:35 -0600

    add cutoff distance to BiasProteinDNAElectrostatics

--- open3SPN2/force/protein_dna.py
+++ open3SPN2/force/protein_dna.py
@@ -186,12 +186,13 @@ class ElectrostaticsProteinDNA(ProteinDNAForce):
 class BiasElectrostaticsProteinDNA(ProteinDNAForce):
     """ Protein-DNA string potential"""
     #k_ebias and center should be inputted
-    def __init__(self, dna, protein, k_ebias,center, k_elec, ldby, forceGroup=16):
+    def __init__(self, dna, protein, k_ebias,center, k_elec, ldby, , cutoff_distance = None, forceGroup=16):
         self.k_ebias = k_ebias
         self.center = center
         self.k_elec = k_elec
         self.ldby = ldby
         self.forceGroup = forceGroup
+        self.cutoff_distance = cutoff_distance
         super().__init__(dna, protein)
 
     def reset(self):
@@ -200,7 +201,7 @@ class BiasElectrostaticsProteinDNA(ProteinDNAForce):
         k_ebias = self.k_ebias
         center = self.center
         ebiasForce = openmm.CustomCVForce(f"0.5*k_ebias*((E_elec-center)/4.184)^2")
-        E_elec = ElectrostaticsProteinDNA(self.dna, self.protein, k = self.k_elec, ldby = self.ldby)
+        E_elec = ElectrostaticsProteinDNA(self.dna, self.protein, k = self.k_elec, ldby = self.ldby, cutoff_distance = self.cutoff_distance)
         elec = E_elec.force
         #ebiasForce.addCollectiveVariable("E_elec", E_elec)
         ebiasForce.addCollectiveVariable("E_elec", elec)    #Is in kJ/mol
